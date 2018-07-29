36b7ef098289cda618645fb820b9c27ccbd44f6d
PJ Panganiban <ppanganiban@godaddy.com>
2018-07-28 21:33:00 -0700

    HistoryModel

---
+++ src/models/HistoryModel.js
@@ -0,0 +1,31 @@
+// @flow
+import { Schema, model } from 'mongoose';
+import { CreateTime, NaturalNumber, User } from '../util/model-helpers';
+
+export default model('history', new Schema({
+  createTime: CreateTime,
+  matchId: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  turn: {
+    ...NaturalNumber,
+    default: undefined,
+    required: true,
+  },
+  userId: {
+    ...User,
+    default: undefined,
+    required: true,
+  },
+  action: {
+    type: String,
+    enum: [],
+    required: true,
+  },
+  undo: {
+    type: Boolean,
+    default: false,
+  },
+}));
