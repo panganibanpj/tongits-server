4858cd17568eed661b34e8abb0362462ff6d8686
PJ Panganiban <ppanganiban@godaddy.com>
2018-07-28 20:58:54 -0700

    consolidate card constants

--- src/constants/cards/SUITS.js
+++
@@ -1,4 +0,0 @@
-export const SPADES = Symbol('Cards/Suits/Spades');
-export const HEARTS = Symbol('Cards/Suits/Hearts');
-export const DIAMONDS = Symbol('Cards/Suits/Diamonds');
-export const CLUBS = Symbol('Cards/Suits/Clubs');
