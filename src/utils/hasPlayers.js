16eacb3ff7706143727dbf206bbea703414d1d04
PJ Panganiban <ppanganiban@godaddy.com>
2018-07-30 22:54:36 -0700

    fix coverage to include untested

--- src/utils/hasPlayers.js
+++ src/utils/hasPlayers.js
@@ -3,14 +3,14 @@ import type { BSONObjectId } from 'mongoose';
 
 type PlayersType = Array<{ userId: BSONObjectId }>;
 
-export const hasPlayer = (players: PlayersType, userId: ?BSONObjectId) => {
+export const hasPlayer = (players: PlayersType, userId: ?BSONObjectId): boolean => {
   if (!userId) return false;
   const userIdString = userId.toString();
   const isGivenUser = playerId => playerId.toString() === userIdString;
   return players.some(({ userId: playerId }) => isGivenUser(playerId));
 };
 
-export default (players: PlayersType, userIds: Array<BSONObjectId> = []) => {
+export default (players: PlayersType, userIds: Array<BSONObjectId> = []): boolean => {
   if (!userIds.length) return false;
   return userIds.every(userId => hasPlayer(players, userId));
 };
